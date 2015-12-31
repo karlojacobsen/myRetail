b62c58224266d76f883ddd6498e7622da9e6c25e
Karl Jacobsen <karlojacobsen@outlook.com>
2015-12-30 22:06:52 -0600

    updated controller mappings

--- src/main/java/myretail/web/ProductController.java
+++ src/main/java/myretail/web/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import myretail.domain.Product;
@@ -17,19 +19,19 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
-	@RequestMapping(value = "/")
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Product> getProducts() {
 		return productService.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Product getProduct(@PathVariable Long id) {
 		Product product = productService.findOne(id);
 		return product;
 	}
 	
-	@RequestMapping(value = "/search/category/{category}")
-	public List<Product> getProductsByCategory(@PathVariable String category) {
+	@RequestMapping(value = "/filter", method = RequestMethod.GET, params = {"category"})
+	public List<Product> getProductsByCategory(@RequestParam("category") String category) {
 		List<Product> products = productService.findByCategory(category);
 		return products;
 	}
