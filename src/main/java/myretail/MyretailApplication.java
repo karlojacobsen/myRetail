a32ea6d7d1ed61587e165de62b3778e092353232
Karl Jacobsen <karlojacobsen@outlook.com>
2015-12-30 18:44:48 -0600

    initial commit

---
+++ src/main/java/myretail/MyretailApplication.java
@@ -0,0 +1,12 @@
+package myretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyretailApplication.class, args);
+	}
+}
